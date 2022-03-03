f698fac761e6cc64ecc0891b85826522f36fc009
tikitiki0370 <78966308+tikitiki0370@users.noreply.github.com>
2022-03-03 22:23:31 +0900

    add home_gui 値をそろえるオプション追加

--- scr/gui/option_gui.py
+++ scr/gui/option_gui.py
@@ -1,8 +1,17 @@
+
 from tkinter import ttk
+import tkinter as tk
 
 class OptionsGui(ttk.Frame):
     def __init__(self,master) -> None:
-        width = 300
-        height = 300
-        super().__init__(master,width=width, height=height)
         
+        width = 600
+        height = 600
+        super().__init__(master,width=width, height=height)
+
+if __name__ == "__main__":
+    
+    root = tk.Tk()
+    root.geometry("600x600")
+    gui = OptionsGui(master=root)
+    gui.mainloop()
